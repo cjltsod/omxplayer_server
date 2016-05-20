04d3dcccf189963ff7cb4b106665fccc4af80425
Chun-Jung Lee <github.tsod@tsod.idv.tw>
2016-05-20 23:38:50 +0800

    add simple play directory method

--- omxplayer_server/__init__.py
+++ omxplayer_server/__init__.py
@@ -8,6 +8,7 @@ def main(global_config, **settings):
     config.include('pyramid_mako')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('play_omx', '/play/{directory}')
     config.include('.modules')
     config.scan()
     return config.make_wsgi_app()
--- omxplayer_server/views.py
+++ omxplayer_server/views.py
@@ -1,6 +1,22 @@
 from pyramid.view import view_config
+import os
 
 
 @view_config(route_name='home', renderer='templates/mytemplate.mako')
 def my_view(request):
     return {'project': 'omxplayer_server'}
+
+
+@view_config(route_name='play_omx', renderer='json')
+def play_omx(request):
+    directory = request.matchdict.get('directory', '20160410_EA')
+    target_dir = '/mnt/RecordingUpload/RecordingUpload/'.format(directory)
+    file_list = list()
+
+    for eachFile in os.listdir(target_dir):
+        if eachFile.split('.')[-1].lower() in ['mts', 'm2ts']:
+            file_path = '{}/{}'.format(target_dir, eachFile)
+            request.registry.settings['omxplayer_playlist_queue'].put(file_path)
+            file_list.append(file_path)
+
+    return dict(playlist=file_list)
