f1ecdd683b4b112fee62c25b76af856411926442
Chun-Jung Lee <github.tsod@tsod.idv.tw>
2016-06-28 03:11:18 +0800

    reorder tux set option

--- reboot.sh
+++ reboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/usr/bin/tmux set-option -g default-shell /bin/bash
 /usr/bin/tmux new-session -d -s omxplayer_server
+/usr/bin/tmux set-option -g default-shell /bin/bash
 /usr/bin/tmux set-option -t omxplayer_server remain-on-exit on
 /usr/bin/tmux send-keys -t omxplayer_server "/bin/bash" C-m
 /usr/bin/tmux send-keys -t omxplayer_server "source /home/pi/.bash_profile" C-m
