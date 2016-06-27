f261dba2590abb418fab08864855b7f68cb7b6ef
Chun-Jung Lee <github.tsod@tsod.idv.tw>
2016-06-28 02:56:54 +0800

    reboot if server down three times

--- reboot.sh
+++ reboot.sh
@@ -4,4 +4,7 @@
 /usr/bin/tmux send-keys -t omxplayer_server "/bin/bash" C-m
 /usr/bin/tmux send-keys -t omxplayer_server "source /home/pi/.bash_profile" C-m
 /usr/bin/tmux send-keys -t omxplayer_server "cd /home/pi/omxplayer_server && pserve production.ini" C-m
+/usr/bin/tmux send-keys -t omxplayer_server "cd /home/pi/omxplayer_server && pserve production.ini" C-m
+/usr/bin/tmux send-keys -t omxplayer_server "cd /home/pi/omxplayer_server && pserve production.ini" C-m
+/usr/bin/tmux send-keys -t omxplayer_server "sudo reboot" C-m
 /usr/bin/tmux set-option -t omxplayer_server remain-on-exit on
