3f16194a9051dee78ab6143aed408f7eb56a089d
Chun-Jung Lee <github.tsod@tsod.idv.tw>
2016-06-27 19:30:09 +0800

    Update reboot script

--- reboot.sh
+++ reboot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source /home/pi/.bash_profile
+/usr/bin/tmux set-option -g default-shell /bin/bash
 /usr/bin/tmux new-session -d -s omxplayer_server
-/usr/bin/tmux send-keys -t omxplayer_server "cd /home/pi/omxplayer_server && $VENV/bin/pserve production.ini" C-m
+/usr/bin/tmux send-keys -t omxplayer_server "/bin/bash" C-m
+/usr/bin/tmux send-keys -t omxplayer_server "source /home/pi/.bash_profile" C-m
+/usr/bin/tmux send-keys -t omxplayer_server "cd /home/pi/omxplayer_server && pserve production.ini" C-m
 /usr/bin/tmux set-option -t omxplayer_server remain-on-exit on
