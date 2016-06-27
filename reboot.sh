5c7999767c650c144c95e1e62b45dc0837e300d7
Chun-Jung Lee <github.tsod@tsod.idv.tw>
2016-06-28 04:29:27 +0800

    Ping before start server

--- reboot.sh
+++ reboot.sh
@@ -4,6 +4,7 @@
 /usr/bin/tmux set-option -t omxplayer_server remain-on-exit on
 /usr/bin/tmux send-keys -t omxplayer_server "/bin/bash" C-m
 /usr/bin/tmux send-keys -t omxplayer_server "source /home/pi/.bash_profile" C-m
+/usr/bin/tmux send-keys -t omxplayer_server "while ! ping -c1 staff.mecpro.com.tw &>/dev/null; do :; done" C-m
 /usr/bin/tmux send-keys -t omxplayer_server "cd /home/pi/omxplayer_server && pserve production.ini" C-m
 /usr/bin/tmux send-keys -t omxplayer_server "cd /home/pi/omxplayer_server && pserve production.ini" C-m
 /usr/bin/tmux send-keys -t omxplayer_server "cd /home/pi/omxplayer_server && pserve production.ini" C-m
